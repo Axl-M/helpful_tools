2ec671d10a9e94569c807234342292cf5021c492
Axl-M <axlmalyasov@gmail.com>
2023-06-22 14:59:17 +1000

    HelpLanguage.py completed

--- HelpLanguage/main.py
+++ HelpLanguage/main.py
@@ -1,5 +1,11 @@
 """
-Морфологический анализатор - приводит слово к нормальной форме.
+Анализирует тексты во всех файлах в указанной директории
+и выводит отчет о частотности для каждого слова (в порядке убывания)
+
+Морфологический анализатор - приводит каждое слово к нормальной форме.
+
+Можно использовать для составления списка слов необходимых для изучения
+иностранного языка исходя из частоты использования данных слов
 
 """
 import os
@@ -12,39 +18,74 @@ FIND_DIRECTORY = 'Test'
 
 morph = pymorphy3.MorphAnalyzer()
 
+
 def walk(directory):
     """
     получить содержимое каждого файла в искомой директории
     :param directory:
     :return:
     """
+    frequency = {}
     for root, dirs, files in os.walk(directory):
         for name in files:
             file = os.path.join(root, name)
             with open(file, encoding='utf-8') as f:
-                analysis(f.read())
-            # print(file)
+                # получить анализ частотности для данного файла
+                new_frequency = analysis(f.read())
+
+            # объединить анализы частотности в общий словарь
+            for word in new_frequency:
+                count = frequency.get(word, 0)
+                frequency[word] = count + new_frequency.get(word)
+
+    print_result(frequency)
 
 
-def analysis(content):
+def analysis(content: str) -> dict:
+    """
+    :param content: Содержимое файла (текст)
+    :return: Возвращает словарь {начальна форма слова: количество вхождений этого слова в тексте}
+    """
     frequency = {}
-    # получить список слов
-    # найти слова начинающиеся с любой из букв русского алфавита, состоящие минимум из одной буквы (+)
+    # получить список слов где каждое
+    # найденное слово начинается с любой из букв русского алфавита, состоящие минимум из одной буквы (+)
     matches = re.findall(r'\b[а-я]+\b', content, re.IGNORECASE)  # \b - граница слова
     # print(content)
     for word in matches:
         word = word.lower()
         # получить нормальную форму слова (именительный падеж, единственное число ....)
         word = morph.parse(word)[0].normal_form
-        # word = morph.parse(word)[0].normal_form
-        # print(morph.parse(word)[0].normal_form)
         # получить количество для слова, если его еще нет присвоить количеству 0
         count = frequency.get(word, 0)
         # Записать в словарь количество дл каждого слова увеличив его на 1
         frequency[word] = count + 1
-    print(frequency)
+    return frequency
+
+
+def print_result(frequency: dict) -> None:
+    """
+    вывести содержимое словаря построчно
+    :param frequency:
+    :return:
+    """
+    frequency = sort(frequency)  # остортировать содержимое словаря
+    # print(frequency)
+    for word in frequency:
+        print(f'{word} -- {frequency[word]}')
+
+
+def sort(dictionary: dict) -> dict:
+    """
+    отсортировать словарь по частотности значений
+    :param dictionary: словарь для сортировки
+    :return: отсортированный по значениям словарь (в порядке убывания)
+    """
+    sorted_dict = {}
+    sorted_keys = sorted(dictionary, key=dictionary.get, reverse=True)
+    for key in sorted_keys:
+        sorted_dict[key] = dictionary[key]
+    return sorted_dict
 
 
 if __name__ == '__main__':
     walk(FIND_DIRECTORY)
-
