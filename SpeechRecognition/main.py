a09ebe09cf2aececfe33feaf58937a1c6bc3ca80
Axl-M <axlmalyasov@gmail.com>
2023-06-26 14:11:25 +1000

    SpeechRecognition completed and tested

--- SpeechRecognition/main.py
+++ SpeechRecognition/main.py
@@ -6,6 +6,18 @@ import speech_recognition as sr
 
 FILE = 'audio.wav'
 
+r = sr.Recognizer()
+with sr.AudioFile(FILE) as source:
+    audio = r.record(source)
+
+try:
+    text = r.recognize_google(audio, language='ru-RU')
+    print(text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 
 if __name__ == "__main__":
     ...
