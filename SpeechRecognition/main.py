bc2d6bc584f91352ed69076fdfe83a1edd6ec94b
Axl-M <axlmalyasov@gmail.com>
2023-06-26 14:26:50 +1000

    SpeechRecognition completed and tested - ok

--- SpeechRecognition/main.py
+++ SpeechRecognition/main.py
@@ -10,6 +10,10 @@ r = sr.Recognizer()
 with sr.AudioFile(FILE) as source:
     audio = r.record(source)
 
+# или так (НЕ ДЕЛАТЬ ТАК - файл не закрывается)
+# source = sr.AudioFile(FILE).__enter__()
+# audio = r.record(source)
+
 try:
     text = r.recognize_google(audio, language='ru-RU')
     print(text)
@@ -19,5 +23,20 @@ except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-if __name__ == "__main__":
-    ...
+'''
+class TestWith:
+    # вызывается в начале оператора with
+    def __enter__(self):
+        print('Вход')
+        return 1    # запишется в test
+
+    # выполняется по окончанию with
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Выход')
+
+
+with TestWith as test:
+    print('Выполнение кода внутри with')
+    print(test)
+
+'''
